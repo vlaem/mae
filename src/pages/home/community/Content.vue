214c008bc2582265b9c52f59f46f0d3c894d8e93
Alvaro Ccatamayo <alvaro.ccatamayo@outlook.com>
2021-11-29 21:30:11 -0500

    Added community tab data

---
+++ src/pages/home/community/Content.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="content" @click="viewContent">
+    <img :src="content.file" />
+    <p>{{ content.title }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ["content"],
+  setup() {
+    const viewContent = () => {
+        v
+    };
+
+    return {
+      viewContent,
+    };
+  },
+};
+</script>
+<style scoped>
+.content {
+  border: 1px solid #ccc;
+  cursor: pointer;
+}
+</style>
