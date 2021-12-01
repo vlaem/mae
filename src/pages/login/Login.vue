19eab9f9f91472e273090b2a25eb79d0773343be
Alvaro Ccatamayo <alvaro.ccatamayo@outlook.com>
2021-11-30 20:07:10 -0500

    Style for login

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -2,15 +2,18 @@
   <div class="container view">
     <form class="login" @submit="login">
       <img src="/logo.png" />
-      <h1>Acceder</h1>
-      <h2>Ir a Storytelling App</h2>
-      <h2>¿Cómo te llamas?</h2>
-
+      <div class="title">
+        <h1>Acceder</h1>
+        <h2>Ir a Storytelling App</h2>
+        <h2>¿Cómo te llamas?</h2>
+      </div>
       <o-input
         v-model="credentials.name"
         placeholder="Ingresar Nombre"
       ></o-input>
-      <o-button type="submit" @click="login">Ingresar</o-button>
+      <div class="actions">
+        <o-button type="submit" @click="login">Ingresar</o-button>
+      </div>
     </form>
   </div>
 </template>
@@ -42,5 +45,24 @@ export default {
   display: flex;
   height: 100vh;
   max-width: 570px;
+}
+
+.login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 1.5rem;
+  width: 100%;
+  margin: auto;
+  gap: 1.3rem;
+}
+
+.title {
+  text-align: center;
+}
+
+h1,
+h2 {
+  margin: 0;
 }
 </style>
